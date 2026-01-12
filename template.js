9dda5809350899434f465c1222daf109f4f98945
Nguyen Duy Linh <user@LinhND.local>
2026-01-12 22:54:12 +0700

    Init template file

---
+++ template.js
@@ -0,0 +1,11 @@
+// version 1.0.0
+
+(function () {
+  const data = {
+
+  };
+  const s = document.createElement("script");
+  s.type = "application/ld+json";
+  s.textContent = JSON.stringify(data);
+  document.head.appendChild(s);
+})();
